9ce92b1700890eaf60eb3a04645e7224d190e3c9
Leo Lu <z@linkyee.com>
2023-05-26 15:04:07 +0800

    fix

--- src/main/java/com/tencent/wxcloudrun/dao/OrderItemMapper.java
+++ src/main/java/com/tencent/wxcloudrun/dao/OrderItemMapper.java
@@ -9,9 +9,9 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface OrderItemMapper {
-    @Insert("INSERT INTO order_item (id, picture, prompt, model, seed, params, custom_type, custom_param, order_id, print_status, picture_base64) " +
+    @Insert("INSERT INTO order_item (id, picture, prompt, model, seed, params, custom_type, custom_param, order_id, print_status, picture_base64，amount, price, total_price) " +
             "VALUES (#{orderItem.id}, #{orderItem.picture}, #{orderItem.prompt}, #{orderItem.model}, #{orderItem.seed}, #{orderItem.params}, " +
-            "#{orderItem.customType}, #{orderItem.customParam}, #{orderItem.orderId}, #{orderItem.printStatus}, #{orderItem.pictureBase64})")
+            "#{orderItem.customType}, #{orderItem.customParam}, #{orderItem.orderId}, #{orderItem.printStatus}, #{orderItem.pictureBase64}, #{orderItem.amount}, #{orderItem.price}, #{orderItem.totalPrice})")
     @Options(useGeneratedKeys = true, keyProperty = "orderItem.id")
     void insertOrderItem(@Param("orderItem") OrderItem orderItem);
 }
